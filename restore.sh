7482b407408ca083e77595d4cd1416761c23f4da
mrbot8 <88660335+mrbot8@users.noreply.github.com>
2021-08-15 07:59:42 -0300

    Add files via upload

---
+++ restore.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+echo -e '\e[1;32m
+
+┳┻┳┻╭━━━━╮╱▔▔▔╲
+┻┳┻┳┃╯╯╭━┫▏╰╰╰▕
+┳┻┳┻┃╯╯┃▔╰┓▔▂▔▕╮
+┻┳┻┳╰╮╯┃┈╰┫╰━╯┏╯
+┳┻┳┻┏╯╯┃╭━╯┳━┳╯
+┻┳┻┳╰━┳╯▔╲╱▔╭╮▔╲
+┳┻┳┻┳┻┃┈╲┈╲╱╭╯╮▕
+┻┳┻┳┻┳┃┈▕╲▂╱┈╭╯╱
+┳┻┳┻┳┻┃'''┈┃┈┃┈'''╰╯
+┻┳┻┳┻┏╯▔'''╰┓┣━┳┫
+┳┻┳┻┳╰┳┳┳'''╯┃┈┃┃
+┻┳┻┳┻┳┃┃┃┈'''┃┈┃┃
+┳┻┳┻┳┻┃┃┃'''┊┃┈┃┃
+┻┳┻┳┻┳┃┃┃┈'''┃┈┃┃.
+┳┻┳┻┳┻┣╋┫'''┊┣━╋┫
+┻┳┻┳┻╭╯╰╰-╭╯━╯.''╰╮
+By Traxnox Bot Guillermo Esta Solo Busca Novia 
+El Tiene 14 Contacto De el Si alguien Quiere ser
+Su Novia  Numero   +593969489665 Ai Esta El Numero,,,,
+
+\e[0m'
+
+rm -r Samu330.json
+
+sleep 2.0
+
+echo -e '\033[0m\033[1;31m
+
+LISTO!! YA PUEDES VOLVER A ESCANEAR EL CODIGO DE BY TRAXNOX
+
+\033[0m'
